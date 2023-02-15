b6acab39674a20aabce276b397dfaec5ce5542f2
Thiago Anjos - TwPC <thiagowaltrick@gmail.com>
2023-02-15 14:20:21 -0300

    exemplos de funcionamento do nest com injeção de dependencia e inversão

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
+import { PostmarkMailService } from './mail/postmark-mail.service';
+//import { AppService } from './app.service';
+import { SMTPMailService } from './mail/smtp-mail.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  //providers: [AppService], //Serviços direto
+  providers: [ //Use assim quando estiver trabalhando com classes Abstratas
+    {
+      provide: MailService, //A classe abstrata
+      useClass: SMTPMailService, //o tipo de implementação
+      //useClass: PostmarkMailService
+    }
+  ]
 })
 export class AppModule {}
