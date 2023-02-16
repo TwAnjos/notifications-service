9255e3bd4646e15ed2d9e5de37d1d1090990d6f0
Thiago Anjos - TwPC <thiagowaltrick@gmail.com>
2023-02-16 19:22:17 -0300

    aula1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MailService } from './mail/mail.service';
-import { PostmarkMailService } from './mail/postmark-mail.service';
+//import { PostmarkMailService } from './mail/postmark-mail.service';
 //import { AppService } from './app.service';
 import { SMTPMailService } from './mail/smtp-mail.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
   //providers: [AppService], //Serviços direto
   providers: [ //Use assim quando estiver trabalhando com classes Abstratas
+    PrismaService,
     {
       provide: MailService, //A classe abstrata
       useClass: SMTPMailService, //o tipo de implementação
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Ativar as validações do NestJS
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
 bootstrap();
